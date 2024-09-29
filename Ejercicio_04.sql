18e1a8526b2b68aadfc20e186b8b9af65f2cc431
Jose Ignacio <jigonzaleztejedor@gmail.com>
2024-09-29 21:04:03 +0200

    Añado Ejercicio 4: Generar el campo vdn_aggregation

---
+++ Ejercicio_04.sql
@@ -0,0 +1,9 @@
+-- Ejercicio 4: Generar el campo vdn_aggregation
+
+SELECT ivr_id
+     , CASE WHEN STARTS_WITH(vdn_label, 'ATC') THEN 'FRONT'
+            WHEN STARTS_WITH(vdn_label, 'TECH') THEN 'TECH'
+            WHEN vdn_label = 'ABSORPTION' THEN 'ABSORPTION'
+            ELSE 'RESTO'
+       END AS vdn_aggregation
+  FROM `keepcoding.ivr_calls`;
