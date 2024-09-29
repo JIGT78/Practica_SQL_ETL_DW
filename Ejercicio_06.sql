5ee112ad13b80f14872ece327f604076572b2928
Jose Ignacio <jigonzaleztejedor@gmail.com>
2024-09-29 22:46:39 +0200

    Añado Ejercicio 6: Generar el campo customer_phone

---
+++ Ejercicio_06.sql
@@ -0,0 +1,8 @@
+-- Ejercicio 6: Generar el campo customer_phone
+
+SELECT
+  calls_ivr_id
+  , MAX(IF(step_customer_phone = 'UNKNOWN' OR step_customer_phone = 'DESCONOCIDO' , NULL , step_customer_phone)) AS step_customer_phone
+FROM `keepcoding.ivr_detail`
+GROUP BY calls_ivr_id
+
