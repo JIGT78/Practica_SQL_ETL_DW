09a20b1ae506fd9f4f1677953671fc65275c1bb0
Jose Ignacio <jigonzaleztejedor@gmail.com>
2024-09-29 22:19:49 +0200

    Añado Ejercicio 5: Generar los campos document_type y document_identification

---
+++ Ejercicio_05.sql
@@ -0,0 +1,8 @@
+-- Ejercicio 5: Generar los campos document_type y document_identification
+
+SELECT
+    calls_ivr_id
+  , MAX(IF(step_document_type = 'UNKNOWN' OR step_document_type = 'DESCONOCIDO' , NULL , step_document_type)) AS step_document_type
+  , MAX(IF(step_document_identification = 'UNKNOWN' OR step_document_identification= 'DESCONOCIDO', NULL , step_document_identification  )) AS step_document_identification
+FROM `keepcoding.ivr_detail`
+GROUP BY calls_ivr_id;
