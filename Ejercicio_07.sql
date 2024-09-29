555042be76082c1ed6808e62b6eb2a45dfcd23ff
Jose Ignacio <jigonzaleztejedor@gmail.com>
2024-09-29 22:51:09 +0200

    Añado Ejercicio 7: Generar el campo billing_acount_id

---
+++ Ejercicio_07.sql
@@ -0,0 +1,9 @@
+-- Ejercicio 7: Generar el campo billing_acount_id
+
+SELECT
+  calls_ivr_id
+  , MAX(IF(step_billing_account_id = 'UNKNOWN' OR step_billing_account_id = 'DESCONOCIDO' , NULL , step_billing_account_id)) AS step_billing_account_id
+FROM `keepcoding.ivr_detail`
+GROUP BY calls_ivr_id
+
+
